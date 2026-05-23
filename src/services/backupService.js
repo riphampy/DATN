fa31d8fceb31ba5bc16526ba17459c76c57b3558
Hiển <whitepeopleosu@gmail.com>
2026-05-23 04:59:51 +0000

    Pending changes exported from your codespace

---
+++ src/services/backupService.js
@@ -0,0 +1,27 @@
+import HTTP from "./httpService";
+import { baseApiUrl } from "../config.js";
+
+// set up Query String
+const apiEndpoint = baseApiUrl + "/system/backup";
+const getAllQuery = apiEndpoint + ":fetchAllVersion";
+const getBackup = baseApiUrl + "/system/restoreDatabase?versionBackup=";
+const restoreQuery = baseApiUrl + "/system/backup:createBackup";
+
+HTTP.handleProtectedAPI();
+
+export function getAllVersionBackup() {
+  return HTTP.GET(getAllQuery);
+}
+export function Backup(position) {
+  return HTTP.GET(`${getBackup}${position}`);
+}
+
+export function Restore() {
+  return HTTP.GET(restoreQuery);
+}
+
+export default {
+  getAllVersionBackup,
+  Backup,
+  Restore,
+};
